ee844a73b573076afa4548d99d3c258f4b8f4e23
Gilson Silva <gilsonsilvati@gmail.com>
2016-09-03 13:35:09 -0300

    Categoria e Subcategorias.

--- src/main/java/br/com/pedidovenda/controller/CadastroProdutoBean.java
+++ src/main/java/br/com/pedidovenda/controller/CadastroProdutoBean.java
@@ -6,10 +6,12 @@ import java.util.List;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.validation.constraints.NotNull;
 
 import br.com.pedidovenda.model.Categoria;
 import br.com.pedidovenda.model.Produto;
 import br.com.pedidovenda.repository.Categorias;
+import br.com.pedidovenda.util.jsf.FacesUtil;
 
 @Named
 @ViewScoped
@@ -21,28 +23,48 @@ public class CadastroProdutoBean implements Serializable {
 	private Categorias categorias;
 	
 	private Produto produto;
+	private Categoria categoriaPai;
 	
 	private List<Categoria> categoriasRaizes;
+	private List<Categoria> subcategorias; 
 	
 	public CadastroProdutoBean() {
 		produto = new Produto();
 	}
 	
 	public void inicializar() {
-		System.out.println("Inicializando...");
-		
-		categoriasRaizes = categorias.raizes(); 
+		if (FacesUtil.isNotisPostback()) {
+			categoriasRaizes = categorias.raizes(); 
+		}
+	}
+	
+	public void carregarSubcategoria() {
+		subcategorias = categorias.subcategoriasDe(categoriaPai);
 	}
 	
 	public void salvar() {
+		System.out.println("Categoria pai selecionada: " + categoriaPai.getDescricao());
+		System.out.println("Subcategoria selecionada: " + produto.getCategoria().getDescricao());
 	}
 
 	public Produto getProduto() {
 		return produto;
 	}
+	
+	@NotNull
+	public Categoria getCategoriaPai() {
+		return categoriaPai;
+	}
+	public void setCategoriaPai(Categoria categoriaPai) {
+		this.categoriaPai = categoriaPai;
+	}
 
 	public List<Categoria> getCategoriasRaizes() {
 		return categoriasRaizes;
+	}
+
+	public List<Categoria> getSubcategorias() {
+		return subcategorias;
 	}
 	
 }
--- src/main/java/br/com/pedidovenda/util/jsf/FacesUtil.java
+++ src/main/java/br/com/pedidovenda/util/jsf/FacesUtil.java
@@ -5,6 +5,14 @@ import javax.faces.context.FacesContext;
 
 public class FacesUtil {
 	
+	public static boolean isPostback() {
+		return FacesContext.getCurrentInstance().isPostback();
+	}
+	
+	public static boolean isNotisPostback() {
+		return !isPostback();
+	}
+	
 	public static void addInfoMessage(String message) {
 		FacesContext.getCurrentInstance().addMessage(null,
 				new FacesMessage(FacesMessage.SEVERITY_INFO, message, message));
