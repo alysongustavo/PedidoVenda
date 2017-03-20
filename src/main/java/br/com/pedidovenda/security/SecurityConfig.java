752efc4ffdbeb71f90175a91aaa3052ef244c776
Gilson Silva <gilsonsilvati@gmail.com>
2017-03-20 19:22:40 -0300

    Adicionando e configurando Spring Security

---
+++ src/main/java/br/com/pedidovenda/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+//package br.com.pedidovenda.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//	
+//	@Bean
+//	public AppUserDetailsService userDetailsService() {
+//		return new AppUserDetailsService();
+//	}
+//	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		
+//	}
+//
+//}
