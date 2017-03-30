525e96ade3cdaa347a9d542542a0688607f33430
Gilson Silva <gilsonsilvati@gmail.com>
2017-03-29 22:05:36 -0300

    Hackeando o código de validação client-side do PrimeFaces

--- src/main/webapp/resources/js/validation.js
+++ src/main/webapp/resources/js/validation.js
@@ -65,3 +65,22 @@ PrimeFaces.validator.SKU = {
 	}
 		
 };
+
+/* Corrigindo bug de versões antigas do PrimeFaces */
+/*
+PrimeFaces.validator.Highlighter.types.onemenu = {
+		highlight : function(a) {
+			a.parent().siblings(".ui-selectonemenu-trigger").addClass(
+					"ui-state-error").parent().addClass(
+					"ui-state-error");
+			PrimeFaces.validator.Highlighter.highlightLabel(a.parent().parent().find('div input'))
+		},
+		unhighlight : function(a) {
+			a.parent().siblings(".ui-selectonemenu-trigger")
+					.removeClass("ui-state-error").parent()
+					.removeClass("ui-state-error");
+			PrimeFaces.validator.Highlighter.unhighlightLabel(a
+					.parent().parent().find('div input'))
+		}
+};
+*/
